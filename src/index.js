2f3a165fd55838bd18211e0dbe1b8902038ea488
Phil Cockfield <phil@cockfield.net>
2015-11-04 21:19:35 +1300

    Calling /user on github using an auth token.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("\nStarted [file-system-github]\n\n");
+import httpPromises from "http-promises";
+
+
+const http = httpPromises
+  .header("Authorization", `token ${ process.env.GITHUB_TOKEN }`)
+  .header("User-Agent", "file-system-github");
+
+
+
+console.log("calling github....");
+http
+  .get("https://api.github.com/user")
+  .then(result => {
+    console.log("result", result);
+  });
--- test/test.js
+++ test/test.js
@@ -1,8 +1,2 @@
 "use strict";
 import { expect } from "chai";
-
-describe("file-system-github tests", () => {
-  it("succeeds", () => {
-    expect(123).to.equal(123);
-  });
-});
