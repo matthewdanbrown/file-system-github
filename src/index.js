3798bbce8c44122731c778aacc60adb1532f93ca
Phil Cockfield <phil@cockfield.net>
2015-11-04 22:37:56 +1300

    Pulling contents.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@ const http = httpPromises
   .header("User-Agent", "file-system-github");
 
 
+const get = (url) => {
+  console.log("");
+  console.log("calling github....");
+  console.log(url);
+  console.log("");
+  http
+    .get(`https://api.github.com/${ url }`)
+    .then((result) => {
+        console.log(result.data);
+        console.log("");
+    })
+    .catch(err => console.log("err", err));
+};
 
-console.log("calling github....");
-http
-  .get("https://api.github.com/user")
-  .then(result => {
-    console.log("result", result);
-  });
+
+
+// get("user");
+get("repos/philcockfield/file-system-github/contents/src?ref=master")
+get("repos/philcockfield/file-system-github/contents/README.md?ref=master")
